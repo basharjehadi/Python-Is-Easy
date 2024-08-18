a47ec21c32a6065d2354db78920c6a527d0eac53
Md Abul bashar jehadi <basharjehadi@gmail.com>
2024-08-18 16:51:57 +0200

    Homework Assignment #1: Variables

--- Variables.py
+++ Variables.py
@@ -1,8 +1,44 @@
-Artist = "Dave Brubeck"
-Genre = "Jazz"
-DurationInSeconds = 328
+# Homework Assignment #1: Variables
+# This file contains a list of attributes that describe my favorite song. 
+# Each attribute is stored in a variable, and the values are printed out.
 
+# Song Information
+Title = "Bohemian Rhapsody"  # The title of the song
+Artist = "Queen"  # The artist who performed the song
+Album = "A Night at the Opera"  # The album where the song is featured
+YearReleased = 1975  # The year the song was released
+Genre = "Rock"  # The genre of the song
+DurationInSeconds = 354  # The duration of the song in seconds
+DurationInMinutes = 5.54  # The duration of the song in minutes
+Label = "EMI"  # The record label that released the song
+Songwriter = "Freddie Mercury"  # The primary songwriter
+Producer = "Roy Thomas Baker"  # The producer of the song
+LeadVocalist = "Freddie Mercury"  # The lead vocalist in the song
+Guitarist = "Brian May"  # The guitarist in the song
+Bassist = "John Deacon"  # The bassist in the song
+Drummer = "Roger Taylor"  # The drummer in the song
+IsLivePerformance = False  # Whether the version is a live performance or not
+IsCoverSong = False  # Whether the song is a cover of another song
+IsGrammyWinner = False  # Whether the song won a Grammy Award
+PopularityScore = 95.7  # The popularity score of the song (out of 100)
+
+# Printing the song attributes
+print(Title)
 print(Artist)
+print(Album)
+print(YearReleased)
 print(Genre)
 print(DurationInSeconds)
+print(DurationInMinutes)
+print(Label)
+print(Songwriter)
+print(Producer)
+print(LeadVocalist)
+print(Guitarist)
+print(Bassist)
+print(Drummer)
+print(IsLivePerformance)
+print(IsCoverSong)
+print(IsGrammyWinner)
+print(PopularityScore)
 
